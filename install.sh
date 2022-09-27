0a680667d46791e1c58bb8a5bb8c403cb99febd5
j-zero <er@j-zero.de>
2022-09-27 12:36:09 +0200

    install settings

--- install.sh
+++ install.sh
@@ -12,3 +12,4 @@ cp -v .zshrc ~/
 cp -v .vimrc ~/
 cp -v .config/terminator/config ~/.config/terminator/config
 cp -v .vim/colors/codedark.vim ~/.vim/colors/codedark.vim
+[[ ! -f ~/.zshrc.settings ]] && cp -v .zshrc.settings ~/
